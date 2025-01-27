8ef8d52449ec9a9cca6f5d86bd605dc86dbe6f49
Enzo Minchaca <enzominchak@gmail.com>
2025-01-27 14:22:55 -0300

    first commit

--- dv_caja/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import caja
-from . import caja_periodo
-from . import caja_secuencia
-from . import account_journal
-from . import account_move
-from . import account_payment
-from . import reporte_caja
